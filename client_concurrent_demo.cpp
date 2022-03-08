9ab2012039671e9817fa043f729e89813d575003
anurag <adhasma1@asu.edu>
2022-03-08 23:27:40 +0000

    added changes to concurrent

--- client_concurrent_demo.cpp
+++ client_concurrent_demo.cpp
@@ -17,6 +17,7 @@ StubClient d(httpclient, JSONRPC_CLIENT_V2);
 void thread() {
 
 }
+
 void execute(ClientReplica r1,ClientReplica r2,ClientReplica r3,const std::string& data, int offset){
   string vote1 = r1.GetVote(data, "123", "dummy", offset,  "01");  
   string vote2 = r2.GetVote(data, "123", "dummy", offset,  "01");
@@ -27,17 +28,12 @@ void execute(ClientReplica r1,ClientReplica r2,ClientReplica r3,const std::strin
       bool s3 = r3.CommitOrAbort("commit", data, "123", "dummy", offset,  "01");
   }
 }
+
 void printresult(ClientReplica r1,ClientReplica r2,ClientReplica r3,int clientNum) {
+  cout<<"Displaying data in replicas for client "<<clientNum<<endl;
   r1.ShowFileContents();
   r2.ShowFileContents();
   r3.ShowFileContents();
-  /*
-  if(s1 && s2 && s3) {
-    cout<<"client " << clientNum << " success"<<endl;
-    } else {
-    cout<<"client " << clientNum << " failure"<<endl;
-  }
-  */
 }
 void bothclientconcFirstChunk() {
     //first client
@@ -46,123 +42,36 @@ void bothclientconcFirstChunk() {
 
     // get votes from all replicas
     ClientReplica r1{addressChunk1.get("host_urls","")[0].asString()};
-    //r1.ShowFileContents();
-
     ClientReplica r2{addressChunk1.get("host_urls","")[1].asString()};
-    //r2.ShowFileContents();
-
     ClientReplica r3{addressChunk1.get("host_urls","")[2].asString()};
-    //r3.ShowFileContents();
+    
     printresult(r1,r2,r3,1);
+
     Json::Value addressChunkrc2_1 = d.FileLookUp(1, "123", "dummy", "01");
 
     // get votes from all replicas
     ClientReplica rc2_1{addressChunkrc2_1.get("host_urls","")[0].asString()};
-    //rc2_1.ShowFileContents();
-
     ClientReplica rc2_2{addressChunkrc2_1.get("host_urls","")[1].asString()};
-    //rc2_2.ShowFileContents();
-
     ClientReplica rc2_3{addressChunkrc2_1.get("host_urls","")[2].asString()};
-    //rc2_3.ShowFileContents();
+    
     printresult(rc2_1,rc2_2,rc2_3,2);
+
     vector<std::thread> threads;
-    static std::string file_data[2] = {"client 1 file data update","client 2 fdu"};
-    static int vals[2] = {1,2};
-    threads.push_back(std::thread(execute,r1,r2,r3,file_data[0],vals[0]));
-    threads.push_back(std::thread(execute,rc2_1,rc2_2,rc2_3,file_data[1],vals[1]));
+
+    static std::string file_data[4] = {"client 1 u1", "client 2 u1", "client2 u2", "client1 u2"};
+    static int offsets[4] = {1,2,1,0};
+
+    threads.push_back(std::thread(execute,r1,r2,r3,file_data[0],offsets[0]));
+    threads.push_back(std::thread(execute,rc2_1,rc2_2,rc2_3,file_data[1],offsets[1]));
+
+    threads.push_back(std::thread(execute,rc2_1,rc2_2,rc2_3,file_data[2],offsets[2]));
+    threads.push_back(std::thread(execute,r1,r2,r3,file_data[3],offsets[3]));
 
     for (auto& t: threads)
       t.join();
+  
     printresult(r1,r2,r3,1);
     printresult(rc2_1,rc2_2,rc2_3,2);
-    /*
-
-    string vote1 = r1.GetVote("test", "123", "dummy", 1,  "01");  
-    string vote2 = r2.GetVote("test", "123", "dummy", 1,  "01");
-    string vote3 = r3.GetVote("test", "123", "dummy", 1,  "01");
-    
-    // send commit/abort to all replicas
-    
-    if(vote1 == "commit" && vote1 == vote2 && vote1 == vote3) {
-      bool s1 = r1.CommitOrAbort("commit", "test", "123", "dummy", 1,  "01");
-      bool s2 = r2.CommitOrAbort("commit", "test", "123", "dummy", 1,  "01");
-      bool s3 = r3.CommitOrAbort("commit", "test", "123", "dummy", 1,  "01");
-
-      r1.ShowFileContents();
-      r2.ShowFileContents();
-      r3.ShowFileContents();
-
-      if(s1 && s2 && s3) {
-        cout<<"client 1 success"<<endl;
-      } else {
-        cout<<"client 1 failure"<<endl;
-      }
-
-    } else {
-      bool s1 = r1.CommitOrAbort("abort", "test", "123", "dummy", 1,  "01");
-      bool s2 = r2.CommitOrAbort("abort", "test", "123", "dummy", 1,  "01");
-      bool s3 = r3.CommitOrAbort("abort", "test", "123", "dummy", 1,  "01");
-
-      if(s1 && s2 && s3) {
-        cout<<"client 1 success"<<endl;
-      } else {
-        cout<<"client 1 failure"<<endl;
-      }
-    }
-    */
-
-
-
-    //second client
-    // get a list of all replica server addresses
-    /*
-    Json::Value addressChunkrc2_1 = d.FileLookUp(1, "123", "dummy", "01");
-
-    // get votes from all replicas
-    ClientReplica rc2_1{addressChunkrc2_1.get("host_urls","")[0].asString()};
-    rc2_1.ShowFileContents();
-
-    ClientReplica rc2_2{addressChunkrc2_1.get("host_urls","")[1].asString()};
-    rc2_2.ShowFileContents();
-
-    ClientReplica rc2_3{addressChunkrc2_1.get("host_urls","")[2].asString()};
-    rc2_3.ShowFileContents();
-    */
-    /*
-
-    vote1 = rc2_1.GetVote("test2", "123", "dummy", 2,  "01");  
-    vote2 = rc2_2.GetVote("test2", "123", "dummy", 2,  "01");
-    vote3 = rc2_3.GetVote("test2", "123", "dummy", 2,  "01");
-
-    // send commit/abort to all replicas
-    if(vote1 == "commit" && vote1 == vote2 && vote1 == vote3) {
-      bool s1 = rc2_1.CommitOrAbort("commit", "test2", "123", "dummy", 2,  "01");
-      bool s2 = rc2_2.CommitOrAbort("commit", "test2", "123", "dummy", 2,  "01");
-      bool s3 = rc2_3.CommitOrAbort("commit", "test2", "123", "dummy", 2,  "01");
-
-      rc2_1.ShowFileContents();
-      rc2_2.ShowFileContents();
-      rc2_3.ShowFileContents();
-
-      if(s1 && s2 && s3) {
-        cout<<"client 2 success"<<endl;
-      } else {
-        cout<<"client 2 failure"<<endl;
-      }
-
-    } else {
-      bool s1 = rc2_1.CommitOrAbort("abort", "test2", "123", "dummy", 2,  "01");
-      bool s2 = rc2_2.CommitOrAbort("abort", "test2", "123", "dummy", 2,  "01");
-      bool s3 = rc2_3.CommitOrAbort("abort", "test2", "123", "dummy", 2,  "01");
-
-      if(s1 && s2 && s3) {
-        cout<<"client 2 success"<<endl;
-      } else {
-        cout<<"client 2 failure"<<endl;
-      }
-    }
-    */
 }
 
 int main() {
